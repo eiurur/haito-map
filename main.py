e42a7294c029103124d188f4d1e54c435da8b7ea
eiurur <eiurur789@gmail.com>
2022-09-07 00:51:34 +0900

    EPS,PBR,PERの追加とyafooqueryとyfinanceの二段階処理で高速化

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import json
 import yfinance as yf
+from yahooquery import Ticker
 
 
 read_file = pd.read_excel(r"tosho.xls")
@@ -14,29 +15,69 @@ stock_tickers = [f"{row[1]}.T" for row in codes.itertuples()]
 stock_names = [f"{row[2]}" for row in codes.itertuples()]
 stock_segments = [f"{row[3]}" for row in codes.itertuples()]
 
+today = datetime.date.today()
+today_yyyymmdd = today.strftime('%Y%m%d')
+db_dpath = f"./db/{today_yyyymmdd}"
+os.makedirs(db_dpath, exist_ok=True)
+
+# dump tickers
+tickers_fpath = os.path.join(db_dpath, f"tickers.json")
+
+sd = None
+if os.path.exists(tickers_fpath):
+    with open(tickers_fpath, 'r', encoding='utf-8') as f:
+        sd = json.load(f)
+else:
+    tickers = Ticker(stock_tickers, progress=True)
+    sd = tickers.summary_detail
+    with open(tickers_fpath, 'w', encoding="utf-8") as f:
+        json.dump(sd, f, indent = 4, ensure_ascii=False)
+
 # dump stocks
-data = [] 
-ticks = " ".join(stock_tickers)
-tickers = yf.Tickers(ticks)
-for idx, (ticker, detail) in enumerate(tickers.tickers.items()):
-    if "lastDividendValue" not in detail.info or detail.info["lastDividendValue"] is None:
+stocks_provide_dividend = [] 
+for idx, (ticker, detail) in enumerate(sd.items()):
+    if "dividendYield" not in detail:
         continue
-    
     name = stock_names[idx]
     segment = stock_segments[idx]
-    dividendRate = detail.info["lastDividendValue"] / detail.info["regularMarketPrice"]
-    dividendYield = detail.info["lastDividendValue"]
-    previousClose = detail.info["previousClose"]
-    regularMarketPrice = detail.info["regularMarketPrice"]
-
-    data.append({
+    stocks_provide_dividend.append({
       "ticker": ticker,
       "name": name,
       "segment": segment,
+    })
+
+print(len(stocks_provide_dividend))
+data = [] 
+for idx, stock in enumerate(stocks_provide_dividend):
+    ticker_info = yf.Ticker(stock["ticker"])
+    if "lastDividendValue" not in ticker_info.info or ticker_info.info["lastDividendValue"] is None:
+        continue
+    
+    dividendRate = ticker_info.info["lastDividendValue"] / ticker_info.info["regularMarketPrice"]
+    dividendYield = ticker_info.info["lastDividendValue"]
+    previousClose = ticker_info.info["previousClose"]
+    regularMarketPrice = ticker_info.info["regularMarketPrice"]
+
+    # https://indepth-markets.com/matplotlib/get_ticker_info/
+    trailingEps = ticker_info.info["trailingEps"] # 実質EPS
+    forwardEps = ticker_info.info["forwardEps"] # 予想EPS
+    trailingPE = ticker_info.info["trailingPE"] # 実質PER
+    forwardPE = ticker_info.info["forwardPE"] # 予想PER
+    pbr = ticker_info.info["priceToBook"]
+
+    data.append({
+      "ticker": stock["ticker"],
+      "name": stock["name"],
+      "segment": stock["segment"],
       "dividendRate": dividendRate,
       "dividendYield": dividendYield,
       "previousClose": previousClose,
       "regularMarketPrice": regularMarketPrice,
+      "trailingEps": trailingEps,
+      "forwardEps": forwardEps,
+      "trailingPE": trailingPE,
+      "forwardPE": forwardPE,
+      "pbr": pbr
     })
 
 today = datetime.date.today()
